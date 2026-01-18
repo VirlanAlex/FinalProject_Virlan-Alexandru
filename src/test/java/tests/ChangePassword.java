4195e7497cbe53bf6d404d9098b5fdbfb9615480
Alex Virlan <virlanalexandru20@yahoo.com>
2026-01-18 13:32:40 +0200

    second commit

--- src/test/java/tests/ChangePassword.java
+++ src/test/java/tests/ChangePassword.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,15 +13,17 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LogIn {
+public class ChangePassword {
     public WebDriver driver;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+
 
     @Test
-    private void signInAccount(){
-        signInAccount("virlanalexandru20@yahoo.com","123sd21123@asdadd2Asdsd");
+    private void signInAccount() {
+        signInAccount("virlanalexandru20@yahoo.com", "123sd21123@asdadd2Asds1");
     }
 
-    public void signInAccount(String signInEmail, String signInPassword){
+    public void signInAccount(String signInEmail, String signInPassword) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("--start-maximized");
@@ -42,6 +45,24 @@ public class LogIn {
         logInButtonElement.click();
         wait.until(ExpectedConditions.urlContains("/account"));
         Assert.assertTrue(driver.getCurrentUrl().contains("/account"), "Login failed: user was not redirected to account page");
+
+        WebElement profileButton = driver.findElement(By.cssSelector(("[data-test='nav-profile']")));
+        profileButton.click();
+
+        WebElement currentPasswordInput = driver.findElement(By.cssSelector("input[data-test='current-password']"));
+        currentPasswordInput.clear();
+        currentPasswordInput.sendKeys("123sd21123@asdadd2Asds1");
+
+        WebElement newPasswordInput = driver.findElement(By.cssSelector("input[data-test='new-password']"));
+        newPasswordInput.clear();
+        newPasswordInput.sendKeys("123sd21123@asdadd2Asds2");
+
+        WebElement confirmNewPasswordInput = driver.findElement(By.cssSelector("input[data-test='new-password-confirm']"));
+        confirmNewPasswordInput.clear();
+        confirmNewPasswordInput.sendKeys("123sd21123@asdadd2Asds2");
+
+        WebElement changePasswordButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[data-test='change-password-submit']")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", changePasswordButton);
 
     }
 }
--- src/test/java/tests/SetUpTwoFactorAuthentication.java
+++ src/test/java/tests/SetUpTwoFactorAuthentication.java
@@ -12,15 +12,16 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LogIn {
+public class SetUpTwoFactorAuthentication {
+
     public WebDriver driver;
 
     @Test
-    private void signInAccount(){
-        signInAccount("virlanalexandru20@yahoo.com","123sd21123@asdadd2Asdsd");
+    private void setUpAuthentication() {
+        signInAccount("virlanalexandru20@yahoo.com", "123sd21123@asdadd2Asdsd");
     }
 
-    public void signInAccount(String signInEmail, String signInPassword){
+    public void signInAccount(String signInEmail, String signInPassword) {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
         options.addArguments("--start-maximized");
@@ -43,5 +44,28 @@ public class LogIn {
         wait.until(ExpectedConditions.urlContains("/account"));
         Assert.assertTrue(driver.getCurrentUrl().contains("/account"), "Login failed: user was not redirected to account page");
 
+        WebElement profileButton = driver.findElement(By.cssSelector(("[data-test='nav-profile']")));
+        profileButton.click();
+
+        By allTextSecret = By.cssSelector("[data-test='totp-secret'] strong");
+        By allTextInput  = By.cssSelector("input[data-test='totp-code']");
+        String secret = wait.until(context -> {
+            WebElement secretText = context.findElement(allTextSecret);
+            String textText = secretText.getAttribute("textContent");
+            if (textText == null) return null;
+            textText = textText.trim();
+            return textText.isEmpty() ? null : textText;
+        });
+        Assert.assertTrue(secret.matches("[A-Z0-9]{8,64}"), "TOTP secret invalid/empty: '" + secret + "'");
+        WebElement input = wait.until(ExpectedConditions.elementToBeClickable(allTextInput));
+        input.clear();
+        input.sendKeys(secret);
+
+        WebElement verifyButtonElement = driver.findElement(By.cssSelector("button[data-test='verify-totp']"));
+        verifyButtonElement.click();
+
+        By textError = By.cssSelector("[data-test='totp-error']");
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(textError));
+        Assert.assertEquals(errorMessage.getText().trim(), "Invalid TOTP code. Please try again.", "TOTP error message is missing or incorrect");
     }
 }
