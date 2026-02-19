5ae2afe99175f6f6e1b87528d61c31f3e38a1b95
Alex Virlan <virlanalexandru20@yahoo.com>
2026-02-19 16:57:51 +0200

    update

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -8,8 +8,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.LogUtility;
 import utils.TestDataLoader;
-import java.time.Duration;
 
+import java.time.Duration;
 
 public class SharedData {
 
@@ -26,17 +26,23 @@ public class SharedData {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
-        options.addArguments("--start-maximized");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
 
-        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        boolean isCi = "true".equalsIgnoreCase(System.getenv("CI"))
+                || "true".equalsIgnoreCase(System.getenv("GITHUB_ACTIONS"));
+
+        boolean headless = Boolean.parseBoolean(System.getProperty("headless", String.valueOf(isCi)));
+
         if (headless) {
-            options.addArguments("--headless=new", "--window-size=1920,1080");
+            options.addArguments("--headless=new");
+        } else {
+            options.addArguments("--start-maximized");
         }
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ZERO);
-
-        // Start determinist: homepage
         driver.get(url("/"));
     }
 
@@ -59,7 +65,6 @@ public class SharedData {
     public WebDriver getDriver() {
         return driver;
     }
-
     public TestDataModel getData() {
         return data;
     }
